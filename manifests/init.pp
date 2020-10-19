3324c4b6115e6d1f0e7880906fd176fb25212151
Jason Galens <bogey.jlg@gmail.com>
2020-10-19 00:13:08 -0400

    heira sucks

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,18 +5,18 @@
 # @example
 #   include autofs
 class autofs (
-    Variant[Array, String] $package_name,
-    String $package_ensure,
-    Optional[String] $package_source,
+    Variant[Array, String] $package_name = $autofs::params::package_name,
+    String $package_ensure = $autofs::params::package_ensure,
+    Optional[String] $package_source = undef,
 
-    Stdlib::Absolutepath $config_file,
-    Hash[String, Scalar] $settings,
-    Hash $parser_settings,
+    Stdlib::Absolutepath $config_file = $autofs::params::config_file,
+    Hash[String, Scalar] $settings = $autofs::params::settings,
+    Hash $parser_settings = $autofs::params::parser_settings,
 
-    Variant[Array, String] $service_name,
-    Stdlib::Ensure::Service $service_ensure,
-    Boolean $service_enable,
-) {
+    Variant[Array, String] $service_name = $autofs::params::service_name,
+    Stdlib::Ensure::Service $service_ensure = $autofs::params::service_ensure,
+    Boolean $service_enable = $autofs::params::service_enable,
+) inherits autofs::params {
     contain 'autofs::install'
     unless $package_ensure == 'absent' {
         contain 'autofs::config'
