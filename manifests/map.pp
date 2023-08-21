622e60a438a676a0498d5563f9562f2997bd5580
Jason <bogey.jlg@gmail.com>
2023-08-21 15:22:36 +0000

    Added Map and Master types

---
+++ manifests/map.pp
@@ -0,0 +1,122 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @param [String] map
+# @param [Stdlib::Absolutepath] mount
+# @param [Variant[Integer, String]] order
+# @param [Hash] settings
+# @param [Stdlib::Absolutepath] master
+# @param [String] owner
+# @param [String] group
+# @param [String] mode
+# @param [Autofs::Type] type
+# @param [Autofs::Format] format
+# @param [Autofs::Options] options
+# @param [Hash[String, Autofs::Mapping] mappings
+# @param [String] content
+# @param [Variant[Array[String], String]] source
+#
+# @example
+#   autofs::map { 'namevar': }
+define autofs::map (
+  String $map = $title,
+  Stdlib::Absolutepath $mount,
+  Variant[Integer, String[1]] $order = '10',
+  Hash $settings = {},
+  Optional[Stdlib::Absolutepath] $master = undef,
+  Optional[String] $owner = undef,
+  Optional[String] $group = undef,
+  Optional[String] $mode = undef,
+  Optional[Autofs::Type] $type = undef,
+  Optional[Autofs::Format] $format = undef,
+  Optional[Autofs::Options] $options = undef,
+  Optional[Hash[String, Autofs::Mapping]] $mappings = undef,
+  Optional[String] $content = undef,
+  Optional[Variant[Array[String], String]] $source = undef,
+) {
+  include 'autofs'
+
+  $settings.each |$key, $value| {
+    if $value {
+      ini_setting { "autofs::${mount}::${key}":
+        ensure  => present,
+        path    => $autofs::config_file,
+        section => $mount,
+        setting => $key,
+        value   => $value,
+      }
+    } else {
+      ini_setting { "autofs::${mount}::${key}":
+        ensure  => absent,
+        path    => $autofs::config_file,
+        section => $mount,
+        setting => $key,
+      }
+    }
+  }
+
+  case $type {
+    'dir': {
+      fail("Autofs::Map['${title}'] cannot be of type ${type}")
+    }
+    'file', undef: {
+      if $mappings {
+        concat { $map:
+          ensure         => present,
+          owner          => pick($owner, 'root'),
+          group          => pick($group, 'root'),
+          mode           => pick($mode, '0644'),
+          ensure_newline => true,
+        }
+        create_resources('autofs::mapping', $mappings)
+      } else {
+        file { $map:
+          ensure  => file,
+          owner   => pick($owner, 'root'),
+          group   => pick($group, 'root'),
+          mode    => pick($mode, '0644'),
+          content => $content,
+          source  => $source,
+        }
+      }
+    }
+    'program', 'exec': {
+      if $mappings {
+        fail("Autofs::Map['${title}'] cannot use mappings if map is executable!")
+      } else {
+        file { $map:
+          ensure  => file,
+          owner   => pick($owner, 'root'),
+          group   => pick($group, 'root'),
+          mode    => pick($mode, '0755'),
+          content => $content,
+          source  => $source,
+        }
+      }
+    }
+    default: {}
+  }
+
+  $_master = pick($master, $autofs::master_map)
+  if defined(Concat[$_master]) {
+    $_formatted_options = Array($options, true).join(',')
+
+    $_entry = $type ? {
+      undef => $format ? {
+        undef => "${mount} ${map} ${_formatted_options}",
+        default => "${mount} file,${format}:${map} ${_formatted_options}",
+      },
+      default => $format ? {
+        undef => "${mount} ${type}:${map} ${_formatted_options}",
+        default => "${mount} ${type},${format}:${map} ${_formatted_options}",
+      }
+    }
+
+    concat::fragment { "${_master}::${map}":
+      target  => $_master,
+      content => $_entry.rstrip(),
+      order   => $order,
+    }
+  }
+}
---
+++ manifests/master.pp
@@ -0,0 +1,108 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @param [String] map
+#   Path or name of the map to include
+#
+# @param [Autofs::Options] options
+#   Options to be applied to the map
+#
+# @param [Boolean] purge
+#   Purge all other maps from directory
+#
+# @param [Variant[Integer, String]] order
+#   Priority of the record in its master
+#
+# @param [Stdlib::Absolutepath] master
+#   The master file/directory that this map belongs to
+#
+# @param [Hash[String, Autofs::Map]] maps
+#   A hash of maps to autogenerate included in this master
+#
+# @param [Autofs::Type] type
+#   The type of master record
+#
+# @param [Autofs::Format] format
+#   The format of the master
+#
+# @param [String] owner
+#   User who owns the master
+#
+# @param [String] group
+#   Group setting for the master
+#
+# @param [String] mode
+#   File permissions mode applied to the master
+#
+# @param [String] content
+#   Raw file content in the master
+#
+# @param [String] source
+#   Source to copy the content from
+define autofs::master (
+  String[1] $map = $title,
+  Autofs::Options $options = [],
+  Boolean $purge = false,
+  Variant[Integer, String[1]] $order = 10,
+  Optional[Stdlib::Absolutepath] $master = undef,
+  Optional[Hash[String[1], Autofs::Map]] $maps = undef,
+  Optional[Autofs::Type] $type = undef,
+  Optional[Autofs::Format] $format = undef,
+  Optional[String] $owner = undef,
+  Optional[String] $group = undef,
+  Optional[String] $mode = undef,
+  Optional[String] $content = undef,
+  Optional[String] $source = undef,
+) {
+  include autofs
+
+  case $type {
+    'dir': {
+      autofs::master::directory { $map:
+        owner => $owner,
+        group => $group,
+        mode  => $mode,
+        purge => $purge,
+      }
+    }
+    'file': {}
+    undef: {
+      if is_absolute_path($map) {
+        autofs::master::file { $map:
+          owner   => $owner,
+          group   => $group,
+          mode    => $mode,
+          maps    => $maps,
+          content => $content,
+          source  => $source,
+        }
+      }
+    }
+    'program', 'exec': {
+      fail("Autofs::Master[${title}] cannot be of type '${type}'")
+    }
+    default: {
+    }
+  }
+
+  $_master = pick($master, $autofs::master_map)
+
+  if defined(Concat[$_master]) {
+    $_options = Array($options, true).join(',')
+
+    $_content = $type ? {
+      undef => "+${map} ${_options}",
+      default => $format ? {
+        undef => "+${type}:${map} ${_options}",
+        default => "+${type},${format}:${map} ${_options}",
+      }
+    }
+
+    concat::fragment { "${_master}::${map}":
+      target  => $_master,
+      content => $_content.rstrip(),
+      order   => $order,
+    }
+  }
+}
